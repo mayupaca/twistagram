012d31cc329aaa13e57c9982b369aacf3ffc859c
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-10-23 10:49:13 -0400

    created user schema

---
+++ models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+// mongooseの中にある.Schemaクラスを使う
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String, // 絶対必要項目にはrequired: true
+      required: true,
+      min: 3,
+      max: 25,
+      unique: true, //他の人と重複できない
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 50,
+    },
+    profilePicture: {
+      type: String,
+      default: "",
+    },
+    coverPicture: {
+      type: String,
+      default: "",
+    },
+    followers: {
+      type: Array,
+      default: [],
+    },
+    followings: {
+      type: Array,
+      default: [],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    desc: {
+      type: String,
+      max: 70,
+    },
+    city: {
+      type: String,
+      max: 50,
+    },
+  },
+  { timestamps: true }
+);
