bb62f5cdb2e0eeac1a3c8daf6dfa59641fbf923f
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-10-23 11:07:32 -0400

    connected node and mongoDB

--- server.js
+++ server.js
@@ -2,25 +2,32 @@ const express = require("express");
 // app関数の中にexpressを格納してexpressの中のクラスやら関数を使えるようにする
 const app = express();
 // routes directoryを使えるようにする
-const userRoute = require('./routes/users')
-const authRoute = require('./routes/auth')
-const postRoute = require('./routes/posts')
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
 // ローカルサーバーのポート
 const PORT = 3000;
+const mongoose = require("mongoose");
+// .config関数
+require("dotenv").config();
 
-
-
-
+// MongoDBと接続
+// .connect関数
+mongoose
+  .connect(process.env.MONGOURL)
+  .then(() => {
+    console.log("Connecting database...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // middleware
 // .useメソッド
 // users.jsの / に /api/usersをデフォルトにした
-app.use('/api/users', userRoute)
-app.use('/api/auth', authRoute)
-app.use('/api/posts', postRoute)
-
-
-
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
 
 // .getメソッドでport3000につながっているか確認
 // root end point
