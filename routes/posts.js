6729055efd24a4aeab73b268004647816419105f
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-10-25 15:02:00 -0400

    created likes

--- routes/posts.js
+++ routes/posts.js
@@ -67,5 +67,38 @@ router.get("/:id", async (req, res) => {
     return res.status(403).json(err);
   }
 });
+// --------------------------------------------------------------------------------
+// Like
+router.put("/:id/like", async (req, res) => {
+  // 自分の投稿もLikeできる
+
+  try {
+    // likeしたい投稿を見つける
+    // req.params.idはlikeしたい投稿のid
+    const post = await Post.findById(req.params.id);
+    // まだ投稿にLikeが押されてなかったら...
+
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({
+        $push: {
+          likes: req.body.userId,
+        },
+      });
+
+      return res.status(200).json("Liked!");
+      // 投稿にLikeされてたら...
+    } else {
+      // LikeしているuserIdを取り除く
+      await post.updateOne({
+        $pull: {
+          likes: req.body.userId,
+        },
+      });
+      return res.status(403).json("Unliked!");
+    }
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
 
 module.exports = router;
